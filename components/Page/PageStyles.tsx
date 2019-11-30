05b4ab88e89db8b22e8dfe35e2bc69f891004677
Kyle Bonar <kylewb2011@gmail.com>
2019-11-30 15:35:59 -0600

    init commit

---
+++ components/Page/PageStyles.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  background-color: ${props => props.theme.siteBackgroundColor};
+  color: ${props => props.theme.black};
+`;
