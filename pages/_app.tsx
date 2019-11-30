4ed730baacd2e67e5d0e3dcb6a6e8d440dd43b04
Kyle Bonar <kylewb2011@gmail.com>
2019-11-30 14:36:14 -0600

    fixing tslint error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import App, { Container } from "next/app";
 import Page from "../components/Page/Page";
 
 class MyApp extends App {
+  props: { Component: any };
+
   public render() {
     const { Component } = this.props;
 
