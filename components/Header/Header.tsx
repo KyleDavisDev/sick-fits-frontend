d54b57e41f386892ba736c7661769380e9d55564
Kyle Bonar <kylewb2011@gmail.com>
2019-11-30 21:21:25 -0600

    progress bar on page load

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,6 +6,16 @@ import NProgress from "nprogress";
 import { Logo, StyledHeader } from "./HeaderStyles";
 import Nav from "../Nav/Nav";
 
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
+
 interface IHeaderProps {}
 
 const Header: React.FunctionComponent<IHeaderProps> = props => {
