44756804deb04f6962544006ba00088a396a5d78
Kyle Bonar <kylewb2011@gmail.com>
2019-12-07 14:35:49 -0600

    Hook up w/ apollo, pass props if needed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,44 @@
 import App from "next/app";
 import Page from "../components/Page/Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
+import { ApolloClient } from "apollo-boost";
 
-class MyApp extends App {
+export interface MyAppProps {
+  apollo: ApolloClient<unknown>;
+}
+
+class MyApp extends App<MyAppProps> {
+  // public async getInitialProps({ Component, ctx }) {
+  //   let pageProps: { query?: string } = {};
+  //   if (Component.getInitialProps) {
+  //     pageProps = await Component.getInitialProps();
+  //   }
+
+  //   // This exposes the queyr to the user
+  //   pageProps.query = ctx.query;
+
+  //   return { pageProps };
+  // }
+
+  static async getInitialProps(appContext) {
+    // calls page's `getInitialProps` and fills `appProps.pageProps`
+    const appProps = await App.getInitialProps(appContext);
+
+    return { ...appProps };
+  }
+
   public render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, apollo } = this.props;
 
     return (
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <ApolloProvider client={apollo}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
