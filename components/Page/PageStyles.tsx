05f388994c4e1a06da7e92e227212d97890c171d
Kyle Bonar <kylewb2011@gmail.com>
2019-12-07 21:47:48 -0600

    styled container div

--- components/Page/PageStyles.tsx
+++ components/Page/PageStyles.tsx
@@ -4,3 +4,9 @@ export const StyledDiv = styled.div`
   background-color: ${props => props.theme.siteBackgroundColor};
   color: ${props => props.theme.black};
 `;
+
+export const StyledInner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
