d33180f94d179f8782e2ace3d4e98658dbc8693d
Kyle Bonar <kylewb2011@gmail.com>
2019-12-07 21:48:01 -0600

    Building out page

--- pages/Sell.tsx
+++ pages/Sell.tsx
@@ -1,9 +1,22 @@
 import * as React from "react";
+import Form from "../components/Form/Form";
 
 export interface ISellProps {}
 
 export default class Sell extends React.Component<ISellProps> {
   public render() {
-    return <div>SELL PAGE</div>;
+    return (
+      <div>
+        <Form>
+          <h2>Sell an Item</h2>
+          <fieldset>
+            <label htmlFor="title">
+              Title
+              <input type="text" id="title" name="title" required></input>
+            </label>
+          </fieldset>
+        </Form>
+      </div>
+    );
   }
 }
