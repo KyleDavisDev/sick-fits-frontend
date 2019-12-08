5e5647a70f1b77eff12bd00aea727951e8f897e0
Kyle Bonar <kylewb2011@gmail.com>
2019-12-07 21:25:14 -0600

    Render image if there is one

--- components/Items/components/Item/Item.tsx
+++ components/Items/components/Item/Item.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Link from "next/link";
 import Title from "./components/Title/Title";
 import PriceTag from "./components/PriceTag/PriceTag";
 import { StyledContainer } from "./ItemStyles";
@@ -9,6 +10,7 @@ interface IItemProp {
     title: string;
     description: string;
     price: number;
+    image?: string;
   };
 }
 
@@ -18,14 +20,17 @@ class Item extends React.Component<IItemProp, {}> {
 
     return (
       <StyledContainer>
+        {item.image && <img src={item.image} alt={item.title} />}
         <Title id={item.id}>{item.title}</Title>
         <PriceTag>{item.price}</PriceTag>
         <p>{item.description}</p>
 
-        <div className="button-list">
-          <Link>
+        <div className="buttonList">
+          <Link href={{ pathname: "update", query: { id: item.id } }}>
             <a>Edit</a>
           </Link>
+          <button>Add to cart</button>
+          <button>delete</button>
         </div>
       </StyledContainer>
     );
