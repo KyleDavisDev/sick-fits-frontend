9a8234c9615c4154619c900641827c5d987c7456
Kyle Bonar <kylewb2011@gmail.com>
2019-12-12 22:03:39 -0600

    Query item from DB, mutate item on save, remove file input

--- components/EditItem/EditItem.tsx
+++ components/EditItem/EditItem.tsx
@@ -1,114 +1,136 @@
 import * as React from "react";
-import { Mutation } from "react-apollo";
+import { Mutation, Query } from "react-apollo";
 import Router from "next/router";
 import gql from "graphql-tag";
 import Form from "../Form/Form";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
-export const CREATE_ITEM_MUTATION = gql`
-  mutation CREATE_ITEM_MUTATION(
-    $title: String!
-    $description: String!
-    $price: Int!
-    $image: String
-    $largeImage: String
-  ) {
-    createItem(
-      title: $title
-      description: $description
-      price: $price
-      image: $image
-      largeImage: $largeImage
-    ) {
+export const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
       id
+      title
+      description
+      price
     }
   }
 `;
 
-interface IEditItemProps {}
+export const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    $id: ID!
+    $title: String
+    $description: String
+    $price: Int
+  ) {
+    updateItem(
+      id: $id
+      title: $title
+      description: $description
+      price: $price
+    ) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+interface IEditItemProps {
+  id: string;
+}
 
 interface IEditItemState {
-  title: string;
-  description: string;
-  image: string;
-  largeImage: string;
-  price: number;
+  title?: string;
+  description?: string;
+  image?: string;
+  largeImage?: string;
+  price?: number;
 
   [name: string]: string | number;
 }
 
 class EditItem extends React.Component<IEditItemProps, IEditItemState> {
-  state: IEditItemState = {
-    title: "",
-    description: "",
-    image: "",
-    largeImage: "",
-    price: 15
-  };
+  state: IEditItemState = {};
 
   public render() {
     return (
       <div>
-        <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
-          {(createItem, payload) => (
-            <Form
-              onSubmit={async e => {
-                await this.onSubmit(e, createItem);
-              }}
-            >
-              <ErrorMessage error={payload.error}></ErrorMessage>
-              <h2>CreateItem an Item</h2>
-              <fieldset disabled={payload.loading} aria-busy={payload.loading}>
-                <label htmlFor="file">
-                  Upload File
-                  <input
-                    type="file"
-                    id="file"
-                    name="file"
-                    placeholder="Upload an image..."
-                    required
-                    onChange={this.uploadFile}
-                  />
-                  {this.state.image && <img src={this.state.image} />}
-                </label>
-                <label htmlFor="title">
-                  Title
-                  <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    required
-                    value={this.state.title}
-                    onChange={this.onChange}
-                  />
-                </label>
-                <label htmlFor="price">
-                  Price
-                  <input
-                    type="number"
-                    id="price"
-                    name="price"
-                    required
-                    value={this.state.price}
-                    onChange={this.onChange}
-                  />
-                </label>
-                <label htmlFor="description">
-                  Description
-                  <textarea
-                    id="description"
-                    name="description"
-                    required
-                    placeholder="Enter a description..."
-                    value={this.state.description}
-                    onChange={this.onChange}
-                  />
-                </label>
-                <button type="submit">Submit</button>
-              </fieldset>
-            </Form>
-          )}
-        </Mutation>
+        <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+          {queryPayload => {
+            if (queryPayload.loading) return <p>Loading...</p>;
+            if (queryPayload.error)
+              return <p>Error: {queryPayload.error.message}</p>;
+            if (!queryPayload.data.item)
+              return <p>No Item found for ID: {this.props.id}</p>;
+            return (
+              <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+                {(updateItem, payload) => (
+                  <Form
+                    onSubmit={async e => {
+                      await this.onSubmit(e, updateItem);
+                    }}
+                  >
+                    <ErrorMessage error={payload.error}></ErrorMessage>
+                    <h2>CreateItem an Item</h2>
+                    <fieldset
+                      disabled={payload.loading}
+                      aria-busy={payload.loading}
+                    >
+                      {/* <label htmlFor="file">
+                        Upload File
+                        <input
+                          type="file"
+                          id="file"
+                          name="file"
+                          placeholder="Upload an image..."
+                          required
+                          onChange={this.uploadFile}
+                        />
+                        {this.state.image && <img src={this.state.image} />}
+                      </label> */}
+                      <label htmlFor="title">
+                        Title
+                        <input
+                          type="text"
+                          id="title"
+                          name="title"
+                          required
+                          defaultValue={queryPayload.data.item.title}
+                          onChange={this.onChange}
+                        />
+                      </label>
+                      <label htmlFor="price">
+                        Price
+                        <input
+                          type="number"
+                          id="price"
+                          name="price"
+                          required
+                          defaultValue={queryPayload.data.item.price}
+                          onChange={this.onChange}
+                        />
+                      </label>
+                      <label htmlFor="description">
+                        Description
+                        <textarea
+                          id="description"
+                          name="description"
+                          required
+                          placeholder="Enter a description..."
+                          defaultValue={queryPayload.data.item.description}
+                          onChange={this.onChange}
+                        />
+                      </label>
+                      <button type="submit">Save Changes</button>
+                    </fieldset>
+                  </Form>
+                )}
+              </Mutation>
+            );
+          }}
+        </Query>
       </div>
     );
   }
@@ -128,10 +150,10 @@ class EditItem extends React.Component<IEditItemProps, IEditItemState> {
     // prevent normal form submission
     e.preventDefault();
     //call api to submit data
-    const res = await fn();
+    const res = await fn({ variables: { id: this.props.id, ...this.state } });
 
     //redirect user
-    Router.push({ pathname: "/item", query: { id: res.data.createItem.id } });
+    // Router.push({ pathname: "/item", query: { id: res.data.createItem.id } });
   };
 
   public uploadFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
