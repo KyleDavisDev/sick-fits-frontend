ae3a7322bde404cf0d30f05726ff29882df88fc1
Kyle Bonar <kylewb2011@gmail.com>
2019-12-14 16:20:32 -0600

    Render errors, render single item

--- components/SingleItem/SingleItems.tsx
+++ components/SingleItem/SingleItems.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import Head from "next/head";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { SingleItemStyles } from "./SingleItemStyles";
 
 export const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
@@ -13,7 +16,9 @@ export const SINGLE_ITEM_QUERY = gql`
   }
 `;
 
-export interface ISingleItemProps {}
+export interface ISingleItemProps {
+  id: string;
+}
 
 export interface ISingleItemState {}
 
@@ -28,6 +33,28 @@ export default class SingleItem extends React.Component<
   }
 
   public render() {
-    return <div>I am a single item</div>;
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {payload => {
+          if (payload.loading) return <p>Loading...</p>;
+          if (payload.error) return <ErrorMessage error={payload.error} />;
+          if (!payload.data.item)
+            return <p>No Item found for ID: {this.props.id}</p>;
+          const { item } = payload.data;
+          return (
+            <SingleItemStyles>
+              <Head>
+                <title>Sick Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemStyles>
+          );
+        }}
+      </Query>
+    );
   }
 }
