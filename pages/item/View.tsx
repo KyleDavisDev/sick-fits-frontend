24a0d2a3119e7a2da354510eebe40ea64de9a844
Kyle Bonar <kylewb2011@gmail.com>
2019-12-14 15:59:32 -0600

    init commit

---
+++ pages/item/View.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import SingleItem from "../../components/SingleItem/SingleItems";
+
+interface IViewProps {}
+
+const View: React.FunctionComponent<IViewProps> = props => {
+  return <SingleItem />;
+};
+
+export default View;
