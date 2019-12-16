35b94ef97cdbbdccf2fb9c8c628072aad2bf7cd4
Kyle Bonar <kylewb2011@gmail.com>
2019-12-15 22:34:08 -0600

    Grab page from query, pass to items component

--- pages/Shop.tsx
+++ pages/Shop.tsx
@@ -1,3 +1,19 @@
-import Home from "./index";
+import * as React from "react";
+import Items from "../components/Items/Items";
 
-export default Home;
+export interface IShopProps {
+  query: { page: string };
+}
+
+export default class Shop extends React.Component<IShopProps> {
+  static getInitialProps({ query }) {
+    return { query };
+  }
+  public render() {
+    return (
+      <div>
+        <Items page={parseFloat(this.props.query.page)} />
+      </div>
+    );
+  }
+}
