ebaca0918fd8f5d47cfc660254692c7d59dc86e9
Kyle Bonar <kylewb2011@gmail.com>
2019-12-15 22:45:17 -0600

    Copied from Sell.tsx

---
+++ pages/sell.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import CreateItem from "../components/CreateItem/CreateItem";
+
+interface ISellProps {}
+
+const Sell: React.FunctionComponent<ISellProps> = props => {
+  return <CreateItem />;
+};
+
+export default Sell;
