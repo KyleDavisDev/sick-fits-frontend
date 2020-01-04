06d823919dcb4de398ebf40df2510aa7065b99d5
Kyle Bonar <kylewb2011@gmail.com>
2020-01-04 11:22:17 -0600

    Init commit

---
+++ components/SignInUser/SignInUser.tsx
@@ -0,0 +1,115 @@
+import * as React from "react";
+import { ApolloConsumer } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "../Form/Form";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import ApolloClient from "apollo-client";
+
+const SIGNINUSER_QUERY = gql`
+  query SIGNINUSER_QUERY($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+export interface ISignInUserProps {}
+
+export interface ISignInUserState {
+  email: string;
+  loading: boolean;
+  error?: string;
+  password: string;
+  [name: string]: string | boolean;
+}
+
+export default class SignInUser extends React.Component<
+  ISignInUserProps,
+  ISignInUserState
+> {
+  state = { password: "", email: "", loading: false, error: "yo" };
+
+  public render() {
+    return (
+      <ApolloConsumer>
+        {client => {
+          return (
+            <Form
+              onSubmit={async e => {
+                e.preventDefault();
+                await this.onFormSubmit(client);
+              }}
+            >
+              <fieldset
+                disabled={this.state.loading}
+                aria-busy={this.state.loading}
+              >
+                <ErrorMessage error={this.state.error} />
+                <h2>Sign In to existing account</h2>
+                <label htmlFor="email">
+                  Email
+                  <input
+                    type="text"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.saveToState}
+                  ></input>
+                </label>
+
+                <label htmlFor="password">
+                  Password
+                  <input
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.saveToState}
+                  ></input>
+                </label>
+
+                <button type="submit">Submit</button>
+              </fieldset>
+            </Form>
+          );
+        }}
+      </ApolloConsumer>
+    );
+  }
+
+  private saveToState = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // grab name and value
+    const name = event.target.name;
+    const val = event.target.value;
+
+    // set state
+    this.setState({ [name]: val });
+  };
+
+  private onFormSubmit = async (client: ApolloClient<any>) => {
+    // set loading state
+    this.setState({ loading: true, error: null });
+
+    client
+      .query({
+        query: SIGNINUSER_QUERY,
+        variables: this.state
+      })
+      .then(({ data, loading }) => {
+        // reset form
+        this.setState({
+          loading,
+          email: "",
+          password: "",
+          error: null
+        });
+      })
+      .catch(err => {
+        console.log(err.message);
+        this.setState({
+          error: err,
+          loading: false
+        });
+      });
+  };
+}
