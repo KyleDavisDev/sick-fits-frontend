920fa0633d724f5060d93e197eaba160186f2ba9
Kyle Bonar <kylewb2011@gmail.com>
2020-01-05 16:16:14 -0600

    init commit

---
+++ components/PleaseSignIn/PleaseSignIn.tsx
@@ -0,0 +1,31 @@
+import { Query } from "react-apollo";
+import { CURRENT_USER_QUERY } from "../User/User";
+import SignInUser from "../SignInUser/SignInUser";
+
+export interface IPleaseSignIn {
+  children: React.Component;
+}
+
+const PleaseSignIn: React.FunctionComponent = (props: IPleaseSignIn) => {
+  return (
+    <Query query={CURRENT_USER_QUERY}>
+      {({ data, loading }) => {
+        // return <p>hey</p>;
+        if (loading) return <p>Loading...</p>;
+
+        if (!data.me) {
+          return (
+            <>
+              <p>Please sign in before you can continue!</p>
+              <SignInUser />
+            </>
+          );
+        }
+
+        return props.children;
+      }}
+    </Query>
+  );
+};
+
+export default PleaseSignIn;
