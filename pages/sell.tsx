444efcf8853b2d7b349f9dec4366deea2a938575
Kyle Bonar <kylewb2011@gmail.com>
2020-01-05 16:16:28 -0600

    Render PleaseSignIn component

--- pages/sell.tsx
+++ pages/sell.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import CreateItem from "../components/CreateItem/CreateItem";
+import PleaseSignIn from "../components/PleaseSignIn/PleaseSignIn";
 
 interface ISellProps {}
 
 const Sell: React.FunctionComponent<ISellProps> = props => {
-  return <CreateItem />;
+  return (
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
+  );
 };
 
 export default Sell;
