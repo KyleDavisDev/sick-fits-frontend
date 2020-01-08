74a1a83ed905031e0f926d530cb6da4483edb06d
Kyle Bonar <kylewb2011@gmail.com>
2020-01-07 21:56:35 -0600

    Open and close component

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,23 +1,51 @@
 import * as React from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import { StyledDiv, StyledHeader, StyledCloseButton } from "./CartStyles";
 import Button from "../Button/Button";
+
+export const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+export const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
 
 export interface ICartProps {}
 
 const Cart: React.FunctionComponent<ICartProps> = (props: ICartProps) => {
   return (
-    <StyledDiv open={false}>
-      <header>
-        <StyledCloseButton title={"close"}>&times;</StyledCloseButton>
-        <StyledHeader>Your Cart</StyledHeader>
-        <p>You have __ items in your cart.</p>
-      </header>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {toggleCart => {
+        return (
+          <Query query={LOCAL_STATE_QUERY}>
+            {({ data }) => {
+              return (
+                <StyledDiv open={data.cartOpen}>
+                  <header>
+                    <StyledCloseButton title={"close"} onClick={toggleCart}>
+                      &times;
+                    </StyledCloseButton>
+                    <StyledHeader>Your Cart</StyledHeader>
+                    <p>You have __ items in your cart.</p>
+                  </header>
 
-      <footer>
-        <p>$10.00</p>
-        <Button onClick={() => {}}>Checkout</Button>
-      </footer>
-    </StyledDiv>
+                  <footer>
+                    <p>$10.00</p>
+                    <Button onClick={() => {}}>Checkout</Button>
+                  </footer>
+                </StyledDiv>
+              );
+            }}
+          </Query>
+        );
+      }}
+    </Mutation>
   );
 };
 
