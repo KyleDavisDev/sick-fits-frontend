35f89982770f9c2e60323feb8c3831e2fa8f1091
Kyle Bonar <kylewb2011@gmail.com>
2020-01-17 15:19:46 -0600

    send mutation to server, refetch queries

--- components/RemoveFromCart/RemoveFromCart.tsx
+++ components/RemoveFromCart/RemoveFromCart.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { CURRENT_USER_QUERY } from "../User/User";
+import { StyledButton } from "./RemoveFromCartStyles";
 
 export const REMOVE_FROM_CART_MUTATION = gql`
   mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
@@ -16,12 +18,17 @@ interface IRemoveFromCartProps {
 
 const RemoveFromCart: React.FunctionComponent<IRemoveFromCartProps> = props => {
   return (
-    <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={props.id}>
+    <Mutation
+      mutation={REMOVE_FROM_CART_MUTATION}
+      variables={{ id: props.id }}
+      refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+    >
       {(removeFromCart, { loading, error }) => {
+        if (error) console.log(error.msg);
         return (
-          <button disabled={loading} onClick={removeFromCart}>
-            Remov{loading ? "ing" : "e"} from cart
-          </button>
+          <StyledButton disabled={loading} onClick={removeFromCart}>
+            &times;
+          </StyledButton>
         );
       }}
     </Mutation>
