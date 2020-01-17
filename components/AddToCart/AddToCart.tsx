3aa8c98bbfad855cd6f03b5b9c889c82816dcc28
Kyle Bonar <kylewb2011@gmail.com>
2020-01-17 11:18:04 -0600

    Refetch current user when item added

--- components/AddToCart/AddToCart.tsx
+++ components/AddToCart/AddToCart.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import { StyledButton } from "./AddToCartStyles";
+import { CURRENT_USER_QUERY } from "../User/User";
 
 export const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!) {
@@ -19,7 +20,11 @@ class AddToCart extends React.Component<IAddToCartProps> {
   render() {
     const { id } = this.props;
     return (
-      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
+      <Mutation
+        mutation={ADD_TO_CART_MUTATION}
+        variables={{ id }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
         {addToCart => {
           return <StyledButton onClick={addToCart}>Add to Cart</StyledButton>;
         }}
