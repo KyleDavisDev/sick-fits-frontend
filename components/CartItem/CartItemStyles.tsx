764cedabc112aae78bd4c61d306aa9ffebd8193d
Kyle Bonar <kylewb2011@gmail.com>
2020-01-17 11:55:58 -0600

    style component

--- components/CartItem/CartItemStyles.tsx
+++ components/CartItem/CartItemStyles.tsx
@@ -1,1 +1,16 @@
-import styled from "../../theme/theme";
+import styled from "styled-components";
+
+export const StyledLi = styled.li`
+  padding: 1rem;
+  border-bottom: 1px solid ${props => props.theme.smoke};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 10px;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`;
