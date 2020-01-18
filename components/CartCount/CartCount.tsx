851cb8527698606c03af6032a9c884a99cdf2b05
Kyle Bonar <kylewb2011@gmail.com>
2020-01-18 13:37:08 -0600

    Animate the unmount and mount

--- components/CartCount/CartCount.tsx
+++ components/CartCount/CartCount.tsx
@@ -1,9 +1,27 @@
 import * as React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { StyledDot, StyledAnimation } from "./CartCountStyles";
 
-interface ICartCountProps {}
+interface ICartCountProps {
+  count: number;
+}
 
 const CartCount: React.FunctionComponent<ICartCountProps> = props => {
-  return <p>yo</p>;
+  return (
+    <StyledAnimation>
+      <TransitionGroup>
+        <CSSTransition
+          unmountOnExit
+          className="count"
+          classNames="count"
+          key={props.count}
+          timeout={{ enter: 400, exit: 400 }}
+        >
+          <StyledDot>{props.count}</StyledDot>
+        </CSSTransition>
+      </TransitionGroup>
+    </StyledAnimation>
+  );
 };
 
 export default CartCount;
