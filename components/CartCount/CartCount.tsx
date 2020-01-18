5c8c0425c510557887712b1b978a55d4259d9f8f
Kyle Bonar <kylewb2011@gmail.com>
2020-01-18 13:04:11 -0600

    Init commit

---
+++ components/CartCount/CartCount.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ICartCountProps {}
+
+const CartCount: React.FunctionComponent<ICartCountProps> = props => {
+  return <p>yo</p>;
+};
+
+export default CartCount;
