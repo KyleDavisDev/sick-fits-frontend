34c7de91d8e629b4bd04bd34f9d3c5f1cda7ecb9
Kyle Bonar <kylewb2011@gmail.com>
2020-01-20 13:43:06 -0600

    Make sure we have a cart

--- components/TakeMyMoney/TakeMyMoney.tsx
+++ components/TakeMyMoney/TakeMyMoney.tsx
@@ -21,6 +21,7 @@ class TakeMyMoney extends React.Component<ITakeMyMoneyProps> {
     return (
       <User>
         {({ data: { me } }) => {
+          if (!me.cart || !me.cart.items.length) return <p>No items</p>;
           const totalItemCount = totalItems(me.cart);
           return (
             <StripeCheckout
