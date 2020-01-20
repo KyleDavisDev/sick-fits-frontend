5ba5d86e9e198f9b77547f73e8f0f616164e3786
Kyle Bonar <kylewb2011@gmail.com>
2020-01-20 12:55:44 -0600

    Render StripeCheckout component and filling out props

--- components/TakeMyMoney/TakeMyMoney.tsx
+++ components/TakeMyMoney/TakeMyMoney.tsx
@@ -5,11 +5,35 @@ import Router from "next/router";
 import NProgress from "nprogress";
 import gql from "graphql-tag";
 import calcTotalPrice from "../../lib/calcTotalPrice";
+import Error from "../ErrorMessage/ErrorMessage";
+import User, { CURRENT_USER_QUERY } from "../User/User";
 
-interface ITakeMyMoneyProps {}
+interface ITakeMyMoneyProps {
+  children: any;
+}
 
-const TakeMyMoney: React.FunctionComponent<ITakeMyMoneyProps> = props => {
-  return <p>yo</p>;
-};
+function totalItems(cart) {
+  return cart.items.reduce((acc, item) => acc + item.quantity, 0);
+}
+
+class TakeMyMoney extends React.Component<ITakeMyMoneyProps> {
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => {
+          return (
+            <StripeCheckout
+              amount={calcTotalPrice(me.cart.items)}
+              name="Sick Fits"
+              description={`Order of ${totalItems(me.cart)}`}
+            >
+              {this.props.children}
+            </StripeCheckout>
+          );
+        }}
+      </User>
+    );
+  }
+}
 
 export default TakeMyMoney;
