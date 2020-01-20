96f5262fca23baf67da828d054350aa5e690fa00
Kyle Bonar <kylewb2011@gmail.com>
2020-01-20 17:03:01 -0600

    single order query, render error or loading if applicable

--- components/Order/Order.tsx
+++ components/Order/Order.tsx
@@ -1,8 +1,33 @@
 import * as React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import moment from "moment";
+import Head from "next/head";
+import formatMoney from "../../lib/formatMoney";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { StyledDiv } from "./OrderStyles";
 
-export const;
+export const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order(id: $id) {
+      id
+      charge
+      total
+      created
+      user {
+        id
+      }
+      items {
+        id
+        price
+        image
+        quantity
+        description
+        title
+      }
+    }
+  }
+`;
 
 export interface IOrderProps {
   id: string;
@@ -18,6 +43,15 @@ export default class Order extends React.Component<IOrderProps, IOrderState> {
   }
 
   public render() {
-    return <div>I am an order!</div>;
+    return (
+      <Query query={SINGLE_ORDER_QUERY} variables={{ id: this.props.id }}>
+        {({ data, error, loading }) => {
+          if (error) return <ErrorMessage error />;
+          if (loading) return <p>Loading...</p>;
+          console.log(data);
+          return <p>yo</p>;
+        }}
+      </Query>
+    );
   }
 }
