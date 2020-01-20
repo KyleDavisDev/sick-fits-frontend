a80dc3bdd51ce5a83bb7070eca33262fe4d0c265
Kyle Bonar <kylewb2011@gmail.com>
2020-01-20 17:38:35 -0600

    Added to query, execute query and render some data

--- components/Orders/Orders.tsx
+++ components/Orders/Orders.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 export const ALL_ORDERS_QUERY = gql`
-  query ALL_ORDERS_QUERY(){
-    orders{
+  query ALL_ORDERS_QUERY {
+    orders(orderBy: created_DESC) {
       id
+      total
+      created
+      items {
+        id
+        title
+        price
+        description
+        quantity
+        image
+      }
     }
   }
 `;
@@ -13,7 +24,18 @@ export const ALL_ORDERS_QUERY = gql`
 interface IOrdersProps {}
 
 const Orders: React.FunctionComponent<IOrdersProps> = props => {
-  return <p>list orders here</p>;
+  return (
+    <Query query={ALL_ORDERS_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <ErrorMessage error />;
+
+        const orders = data.orders;
+
+        return <div>you have {orders.length} orders!</div>;
+      }}
+    </Query>
+  );
 };
 
 export default Orders;
