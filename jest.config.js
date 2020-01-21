a1e585cf77b3023b2d419f0dff3f8fc5960fceab
Kyle Bonar <kylewb2011@gmail.com>
2020-01-20 23:38:38 -0600

    init commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  setupFilesAfterEnv: [require.resolve("./jest.setup.ts")],
+  snapshotSerializers: ["enzyme-to-json"],
+  roots: ["<rootDir>/components"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"]
+};
