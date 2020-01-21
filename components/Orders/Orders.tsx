08ff1604ec65aef6dc1aa9c509e32cc31fddd1e1
Kyle Bonar <kylewb2011@gmail.com>
2020-01-20 18:08:42 -0600

    Render info for each order

--- components/Orders/Orders.tsx
+++ components/Orders/Orders.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import Link from "next/link";
+import moment from "moment";
+import { StyledUl, OrderItemStyles } from "./OrdersStyles";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import formatMoney from "../../lib/formatMoney";
 
 export const ALL_ORDERS_QUERY = gql`
   query ALL_ORDERS_QUERY {
@@ -32,7 +36,53 @@ const Orders: React.FunctionComponent<IOrdersProps> = props => {
 
         const orders = data.orders;
 
-        return <div>you have {orders.length} orders!</div>;
+        return (
+          <div>
+            <h2>You have {orders.length} orders!</h2>
+            <StyledUl>
+              {orders.map(order => {
+                return (
+                  <OrderItemStyles key={order.id}>
+                    <Link
+                      href={{
+                        pathname: "order/view",
+                        query: { id: order.id }
+                      }}
+                    >
+                      <a>
+                        <div className="order-meta">
+                          <p>
+                            {order.items.reduce((accum, item) => {
+                              return accum + item.quantity;
+                            }, 0)}{" "}
+                            Items
+                          </p>
+                          <p>{order.items.length} Products</p>
+                          <p>
+                            Created at{" "}
+                            {moment(order.created * 1000).format("LLL")}
+                          </p>
+                          <p>{formatMoney(order.total)}</p>
+                        </div>
+                        <div className="images">
+                          {order.items.map(item => {
+                            return (
+                              <img
+                                src={item.image}
+                                key={item.id}
+                                alt={item.description}
+                              />
+                            );
+                          })}
+                        </div>
+                      </a>
+                    </Link>
+                  </OrderItemStyles>
+                );
+              })}
+            </StyledUl>
+          </div>
+        );
       }}
     </Query>
   );
