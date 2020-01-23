b60b1597ea560a45c93f98a27e60c5a30f50c675
Kyle Bonar <kylewb2011@gmail.com>
2020-01-22 22:20:01 -0600

    Added test for Title and Pricetag, finished img test

--- components/Item/Item.test.tsx
+++ components/Item/Item.test.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Item from "./Item";
 import { shallow } from "enzyme";
+import formatMoney from "../../lib/formatMoney";
 
 const fakeSkip = 3;
 const fakeFirst = 4;
@@ -13,10 +14,23 @@ const fakeItem = {
 };
 
 describe("<Item />", () => {
+  it("renders and the pricetag and title", () => {
+    const wrapper = shallow(
+      <Item skip={fakeSkip} first={fakeFirst} item={fakeItem} />
+    );
+    const PriceTag = wrapper.find("PriceTag").dive();
+    expect(PriceTag.text()).toEqual(formatMoney(fakeItem.price));
+
+    const Title = wrapper.find("Title").dive();
+    expect(Title.find("a").text()).toEqual(fakeItem.title);
+  });
+
   it("renders image", () => {
     const wrapper = shallow(
       <Item skip={fakeSkip} first={fakeFirst} item={fakeItem} />
     );
-    console.log(wrapper.debug());
+    const img = wrapper.find("img");
+    expect(img.props().src).toEqual(fakeItem.image);
+    expect(img.props().alt).toEqual(fakeItem.title);
   });
 });
