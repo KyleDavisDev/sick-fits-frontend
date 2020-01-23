dbd4cd741377f124efc2a7ce832cef0f5ee5a87a
Kyle Bonar <kylewb2011@gmail.com>
2020-01-22 22:19:24 -0600

    added test environment

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFilesAfterEnv: [require.resolve("./jest.setup.ts")],
   snapshotSerializers: ["enzyme-to-json"],
   roots: ["<rootDir>/components", "<rootDir>/lib"],
+  testEnvironment: "node",
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
