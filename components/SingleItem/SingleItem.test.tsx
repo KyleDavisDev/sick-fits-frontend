9f5226bc4b5e45132c4d7a64ed7d49fb52632c6d
Kyle Bonar <kylewb2011@gmail.com>
2020-01-25 13:59:33 -0600

    init commit

---
+++ components/SingleItem/SingleItem.test.tsx
@@ -0,0 +1,26 @@
+import "jsdom-global/register";
+import * as React from "react";
+import { mount } from "enzyme";
+import wait from "waait";
+import SingleItem, { SINGLE_ITEM_QUERY } from "./SingleItems";
+import { MockedProvider } from "react-apollo/test-utils";
+import { fakeItem } from "../../lib/testUtils";
+
+describe("<SingleItem />", () => {
+  const item = fakeItem();
+  const mocks = [
+    {
+      request: { query: SINGLE_ITEM_QUERY, variables: { id: item.id } },
+      result: { data: { item } }
+    }
+  ];
+
+  it("renders", () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id={item.id} />
+      </MockedProvider>
+    );
+    expect(wrapper).toBeTruthy();
+  });
+});
