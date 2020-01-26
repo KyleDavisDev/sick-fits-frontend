3a53fd71c5c4647ac6e0547b321db939145e0af5
Kyle Bonar <kylewb2011@gmail.com>
2020-01-26 12:06:05 -0600

    init commit

---
+++ components/Pagination/Pagination.test.tsx
@@ -0,0 +1,36 @@
+import "jsdom-global/register";
+import * as React from "react";
+import { mount } from "enzyme";
+import wait from "waait";
+import { MockedProvider } from "react-apollo/test-utils";
+import Pagination, { PAGINATION_QUERY } from "./Pagination";
+
+const page = 5;
+
+function makeMocksFor(length: number) {
+  return [
+    {
+      request: { query: PAGINATION_QUERY },
+      result: {
+        data: {
+          itemsConnection: {
+            __typename: "aggregate",
+            aggregate: { __typename: "count", count: length }
+          }
+        }
+      }
+    }
+  ];
+}
+
+describe("<Pagination />", () => {
+  it("renders", () => {
+    const wrapper = mount(
+      <MockedProvider mocks={makeMocksFor(page)}>
+        <Pagination page={page} />
+      </MockedProvider>
+    );
+
+    expect(wrapper).toBeTruthy();
+  });
+});
