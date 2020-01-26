70b1841b505656cb00575cc3b8588360e25b3ea4
Kyle Bonar <kylewb2011@gmail.com>
2020-01-26 13:34:01 -0600

    test for uploading an image

--- components/CreateItem/CreateItem.test.tsx
+++ components/CreateItem/CreateItem.test.tsx
@@ -8,13 +8,6 @@ import { fakeItem } from "../../lib/testUtils";
 
 // mock the global fetch API
 const dogImage = "https://dog.com/dog.jpg";
-const globalAny: any = global;
-globalAny.fetch = jest.fn().mockResolvedValue({
-  json: () => ({
-    secure_url: dogImage,
-    eager: [{ secure_url: dogImage }]
-  })
-});
 
 describe("<CreateItem />", () => {
   it("renders", () => {
@@ -36,5 +29,42 @@ describe("<CreateItem />", () => {
 
     const form = wrapper.find("form");
     expect(form).toMatchSnapshot();
+  });
+
+  it("uploads an image when changed", async () => {
+    // mock fetch
+    // @ts-ignore
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => ({
+        secure_url: dogImage,
+        eager: [{ secure_url: dogImage }]
+      })
+    });
+
+    const wrapper = mount(
+      <MockedProvider>
+        <CreateItem />
+      </MockedProvider>
+    );
+
+    const input = wrapper.find("input[type='file']");
+    input.simulate("change", { target: { files: [dogImage] } });
+
+    // let component update/load
+    await wait();
+    wrapper.update();
+
+    // grab component
+    const component = wrapper.find("CreateItem").instance() as CreateItem;
+
+    // check state
+    expect(component.state.image).toEqual(dogImage);
+    expect(component.state.largeImage).toEqual(dogImage);
+    // @ts-ignore
+    expect(global.fetch).toHaveBeenCalled();
+
+    // reset the global fetch
+    // @ts-ignore
+    global.fetch.mockReset();
   });
 });
