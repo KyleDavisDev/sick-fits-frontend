0e08d8fcff17a213c2919cfb6c009b616aec4ea4
Kyle Bonar <kylewb2011@gmail.com>
2020-01-26 12:43:45 -0600

    init commit

---
+++ components/RequestReset/RequestReset.test.tsx
@@ -0,0 +1,30 @@
+import "jsdom-global/register";
+import * as React from "react";
+import { mount } from "enzyme";
+import wait from "waait";
+import { MockedProvider } from "react-apollo/test-utils";
+import RequestReset, { REQUEST_RESET_QUERY } from "./RequestReset";
+
+describe("<RequestReset />", () => {
+  const mocks = [
+    {
+      request: {
+        query: REQUEST_RESET_QUERY,
+        variables: { email: "test@email.com" }
+      },
+      result: {
+        data: { requestReset: { __typename: "message", message: "success" } }
+      }
+    }
+  ];
+
+  it("renders", () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <RequestReset />
+      </MockedProvider>
+    );
+
+    expect(wrapper).toBeTruthy();
+  });
+});
