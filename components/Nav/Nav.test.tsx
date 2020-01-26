b8f50fa40c00d3426490f20b480fa2af8b496e36
Kyle Bonar <kylewb2011@gmail.com>
2020-01-25 22:48:02 -0600

    testing snapshot

--- components/Nav/Nav.test.tsx
+++ components/Nav/Nav.test.tsx
@@ -31,4 +31,38 @@ describe("<Nav />", () => {
 
     expect(wrapper).toBeTruthy();
   });
+
+  it("matches snapshot", async () => {
+    // check signed out user first
+    let wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <Nav></Nav>
+      </MockedProvider>
+    );
+
+    // allow component to load/update
+    await wait();
+    wrapper.update();
+
+    let nav = wrapper.find('[data-test="nav"]');
+
+    // check snapshot
+    expect(nav).toMatchSnapshot();
+
+    // mount component w/ signed in user
+    wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <Nav></Nav>
+      </MockedProvider>
+    );
+
+    // allow component to load/update
+    await wait();
+    wrapper.update();
+
+    nav = wrapper.find('[data-test="nav"]');
+
+    // check snapshot
+    expect(nav).toMatchSnapshot();
+  });
 });
