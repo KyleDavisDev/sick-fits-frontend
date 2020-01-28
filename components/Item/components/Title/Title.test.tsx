c6a3322de3cfcaab3d969fda0af35cf3efc6db13
Kyle Bonar <kylewb2011@gmail.com>
2020-01-28 15:16:56 -0600

    Code organizing, test for correct hyperlink

--- components/Item/components/Title/Title.test.tsx
+++ components/Item/components/Title/Title.test.tsx
@@ -1,35 +1,57 @@
+import "jsdom-global/register";
 import * as React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Title from "./Title";
 
 describe("<Title />", () => {
+  let title = "Here is a test title";
+  let id = "abc123";
+
   it("renders", () => {
-    const wrapper = shallow(<Title id={"123"}>test</Title>);
+    const wrapper = shallow(<Title id={id}>{title}</Title>);
 
     expect(wrapper).toBeTruthy();
   });
 
   it("matches snapshot", () => {
-    const wrapper = shallow(<Title id={"123"}>test</Title>);
+    const wrapper = shallow(<Title id={id}>{title}</Title>);
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it("renders the expected title", () => {
-    let title = "Here is a test title";
-    const wrapper = shallow(<Title id={"123"}>{title}</Title>);
+    let fakeTitle = title;
+    const wrapper = shallow(<Title id={id}>{fakeTitle}</Title>);
 
     // check title
-    expect(wrapper.find("a").text()).toEqual(title);
+    expect(wrapper.find("a").text()).toEqual(fakeTitle);
 
     // update title, check again
-    title = "Another title";
-    wrapper.setProps({ children: title });
-    expect(wrapper.find("a").text()).toEqual(title);
+    fakeTitle = "Another title";
+    wrapper.setProps({ children: fakeTitle });
+    expect(wrapper.find("a").text()).toEqual(fakeTitle);
 
     // update title, check again
-    title = "Lets go again!!";
-    wrapper.setProps({ children: title });
-    expect(wrapper.find("a").text()).toEqual(title);
+    fakeTitle = "Lets go again!!";
+    wrapper.setProps({ children: fakeTitle });
+    expect(wrapper.find("a").text()).toEqual(fakeTitle);
+  });
+
+  it("renders a link with the attached id", () => {
+    let fakeID = id;
+    const wrapper = mount(<Title id={fakeID}>{title}</Title>);
+
+    // check href
+    expect(wrapper.find("a").prop("href")).toContain(fakeID);
+
+    // update title, check again
+    fakeID = "asbasbd";
+    wrapper.setProps({ id: fakeID });
+    expect(wrapper.find("a").prop("href")).toContain(fakeID);
+
+    // update title, check again
+    fakeID = "123asdfaasdff_ASdfasdf";
+    wrapper.setProps({ id: fakeID });
+    expect(wrapper.find("a").prop("href")).toContain(fakeID);
   });
 });
