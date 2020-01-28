54ef78b8c8b45db2c0b1078eb6d391f21eb6ee86
Kyle Bonar <kylewb2011@gmail.com>
2020-01-28 14:48:39 -0600

    test for displaying value

--- components/Item/components/PriceTag/PriceTag.test.tsx
+++ components/Item/components/PriceTag/PriceTag.test.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 import PriceTag from "./PriceTag";
+import formatMoney from "../../../../lib/formatMoney";
 
 describe("<PriceTag />", () => {
   it("renders", () => {
@@ -13,5 +14,23 @@ describe("<PriceTag />", () => {
     const wrapper = shallow(<PriceTag>{0}</PriceTag>);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders correct amount", () => {
+    let number = Math.random() * 100;
+    const wrapper = shallow(<PriceTag>{number}</PriceTag>);
+
+    // check once
+    expect(wrapper.text()).toEqual(formatMoney(number));
+
+    // update number and check again
+    number = Math.random() * 100;
+    wrapper.setProps({ children: number });
+    expect(wrapper.text()).toEqual(formatMoney(number));
+
+    // update number and check again
+    number = Math.random() * 100;
+    wrapper.setProps({ children: number });
+    expect(wrapper.text()).toEqual(formatMoney(number));
   });
 });
