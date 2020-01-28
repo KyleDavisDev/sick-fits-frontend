a1fae0e311b30ed25c017d838ab5559abcb38aef
Kyle Bonar <kylewb2011@gmail.com>
2020-01-28 15:26:42 -0600

    init commit

---
+++ components/CreateUser/CreateUser.test.tsx
@@ -0,0 +1,18 @@
+import "jsdom-global/register";
+import * as React from "react";
+import { mount } from "enzyme";
+import wait from "waait";
+import { MockedProvider } from "react-apollo/test-utils";
+import CreateUser, { CREATEUSER_MUTATION } from "./CreateUser";
+
+describe("<CreateUser />", () => {
+  it("renders", () => {
+    const wrapper = mount(
+      <MockedProvider>
+        <CreateUser />
+      </MockedProvider>
+    );
+
+    expect(wrapper).toBeTruthy();
+  });
+});
