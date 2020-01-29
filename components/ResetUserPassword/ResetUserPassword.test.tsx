f375ed63a620f94be26d2371dd0bf79365e5cb3f
Kyle Bonar <kylewb2011@gmail.com>
2020-01-29 09:44:34 -0600

    test for mutation execution on submission and apollo store updates

--- components/ResetUserPassword/ResetUserPassword.test.tsx
+++ components/ResetUserPassword/ResetUserPassword.test.tsx
@@ -10,6 +10,7 @@ import ResetUserPassword, {
 import { CURRENT_USER_QUERY } from "../User/User";
 import { fakeUser, simulateInputChange } from "../../lib/testUtils";
 
+const me = fakeUser();
 const fakePassword = "123";
 let fakeConfirmPassword = "123";
 const fakeResetToken = "asbaafsdfasdf_1231";
@@ -33,6 +34,14 @@ const mocks = [
           name: "SampleName"
         }
       }
+    }
+  },
+  {
+    request: {
+      query: CURRENT_USER_QUERY
+    },
+    result: {
+      data: { me }
     }
   }
 ];
@@ -78,5 +87,41 @@ describe("<ResetUserPassword />", () => {
     expect(wrapper.find("input[name='confirmPassword']").prop("value")).toEqual(
       fakeConfirmPassword
     );
+  });
+
+  it("calls mutation on form submit and updates apollo client", async () => {
+    let apolloClient;
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client;
+            return <ResetUserPassword resetToken={fakeResetToken} />;
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    );
+
+    // let component fully render/load
+    await wait();
+    wrapper.update();
+
+    // simulate user input
+    simulateInputChange(wrapper, "password", fakePassword);
+    simulateInputChange(wrapper, "confirmPassword", fakeConfirmPassword);
+
+    // let component fully render/load
+    await wait();
+    wrapper.update();
+
+    // submit form
+    wrapper.find("form").simulate("submit");
+    await wait();
+
+    // query user out of apolloClient
+    const user = await apolloClient.query({ query: CURRENT_USER_QUERY });
+
+    //check if our user made it to the store!
+    expect(user.data.me).toMatchObject(me);
   });
 });
