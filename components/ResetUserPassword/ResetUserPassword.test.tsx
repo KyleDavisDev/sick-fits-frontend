3bbd0a221c3b8af0b380510d34f65fc47cccfbaf
Kyle Bonar <kylewb2011@gmail.com>
2020-01-29 09:04:38 -0600

    test for input boxes to update

--- components/ResetUserPassword/ResetUserPassword.test.tsx
+++ components/ResetUserPassword/ResetUserPassword.test.tsx
@@ -1,6 +1,6 @@
 import "jsdom-global/register";
 import * as React from "react";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import wait from "waait";
 import { MockedProvider } from "react-apollo/test-utils";
 import { ApolloConsumer } from "react-apollo";
@@ -10,11 +10,19 @@ import ResetUserPassword, {
 import { CURRENT_USER_QUERY } from "../User/User";
 import { fakeUser, simulateInputChange } from "../../lib/testUtils";
 
+const fakePassword = "123";
+let fakeConfirmPassword = "123";
+const fakeResetToken = "asbaafsdfasdf_1231";
+
 const mocks = [
   {
     request: {
       query: RESET_USER_PASSWORD_MUTATION,
-      variables: { resetToken: "abc", password: "123", confirmPassword: "123" }
+      variables: {
+        resetToken: fakeResetToken,
+        password: fakePassword,
+        confirmPassword: fakeConfirmPassword
+      }
     },
     result: {
       data: {
@@ -33,7 +41,7 @@ describe("<ResetUserPassword />", () => {
   it("renders and matches snapshot", () => {
     const wrapper = mount(
       <MockedProvider mocks={mocks}>
-        <ResetUserPassword resetToken={"abc"} />
+        <ResetUserPassword resetToken={fakeResetToken} />
       </MockedProvider>
     );
 
@@ -42,5 +50,33 @@ describe("<ResetUserPassword />", () => {
 
     // check snapshot
     expect(wrapper.find("form")).toMatchSnapshot();
+  });
+
+  it("text boxes update with user input", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ResetUserPassword resetToken={fakeResetToken} />
+      </MockedProvider>
+    );
+
+    // let component fully render/load
+    await wait();
+    wrapper.update();
+
+    // simulate user input
+    simulateInputChange(wrapper, "password", fakePassword);
+    simulateInputChange(wrapper, "confirmPassword", fakeConfirmPassword);
+
+    // let component fully render/load
+    await wait();
+    wrapper.update();
+
+    // check input box
+    expect(wrapper.find("input[name='password']").prop("value")).toEqual(
+      fakePassword
+    );
+    expect(wrapper.find("input[name='confirmPassword']").prop("value")).toEqual(
+      fakeConfirmPassword
+    );
   });
 });
